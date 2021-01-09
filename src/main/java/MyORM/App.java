fdc9ebda39b12dfaa2c89da86aa69adcd20de50c
DESKTOP-576RT13\quang <quangthien.tiit@gmail.com>
2021-01-09 14:25:37 +0700

    + mapWithoutRelationship method.

--- src/main/java/MyORM/App.java
+++ src/main/java/MyORM/App.java
@@ -1,5 +1,12 @@
 package MyORM;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import MyORM.Common.Mapper;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println();
+        try {
+            String conn = "jdbc:mysql://localhost:3306/mydb?user=root&password=qwerty@12";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(conn);
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM users");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
--- src/main/java/MyORM/Common/Mapper.java
+++ src/main/java/MyORM/Common/Mapper.java
@@ -1,5 +1,48 @@
 package MyORM.Common;
 
-public abstract class Mapper<T> {
-    
+import java.lang.reflect.Field;
+import java.sql.ResultSet;
+
+import MyORM.Annotations.Column;
+import MyORM.Annotations.PrimaryKey;
+import MyORM.Exceptions.MapDataException;
+
+public abstract class Mapper {
+
+    public <T> T mapWithoutRelationship(ResultSet rs, Class<T> entityClass) {
+        try {
+            T object = entityClass.getDeclaredConstructor().newInstance();
+            Field[] fields = entityClass.getDeclaredFields();
+
+            for (Field field : fields) {
+                Column column = field.getAnnotation(Column.class);
+                PrimaryKey primaryKey = field.getAnnotation(PrimaryKey.class);
+
+                if (column != null) {
+                    field.setAccessible(true);
+                    field.set(object, rs.getObject(column.name()));
+                }
+
+                if (primaryKey != null) {
+                    field.setAccessible(true);
+                    field.set(object, rs.getObject(primaryKey.name()));
+                }
+            }
+
+            return object;
+        } catch (Exception e) {
+            throw new MapDataException(e.getMessage());
+        }
+    }
+
+    public <T> T mapWithRelationship(Connection con, ResultSet rs, Class<T> entityClass) {
+
+        //Map all column annotation
+        T obj = mapWithoutRelationship(rs, entityClass);
+
+        return obj;
+    }
+
+    protected abstract <T> void mapOneToMany(Connection con, ResultSet rs, Class<T> entityClass);
+    protected abstract <T> void mapManyToMany(Connection con, ResultSet rs, Class<T> entityClass);
 }
